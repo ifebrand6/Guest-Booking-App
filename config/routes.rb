cd2f87735ed2ee206b4752a308d882911668a75e
udeogu ifechuwku <ifebrand6@gmail.com>
2020-11-21 23:50:19 +0100

    created dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   get 'welcome', to: 'sessions#welcome'
+  get 'dashboard', to: 'admins#dashboard'
   resources :messages
   root to: "messages#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
